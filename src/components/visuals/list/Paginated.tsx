1c531de8254fa9048f056680b69030ce20f73f3b
DavidFFDAW <davidferflo2@gmail.com>
2024-11-15 16:32:23 +0100

    fix: build error fix

--- src/components/visuals/list/Paginated.tsx
+++ src/components/visuals/list/Paginated.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 interface Props {
     items: number;
@@ -16,7 +15,11 @@ export default function Paginated({ items, list, page, className }: Props) {
 
     return (
         <div className="paginated paginated-module">
-            <div data-items={items} data-page={initialPage} className={['paginated', className].join(' ')}>
+            <div
+                data-items={items}
+                data-page={initialPage}
+                className={["paginated", className].join(" ")}
+            >
                 {renderList.map((item, indx) => (
                     <Link
                         to={`/items/item/${item.en}`}
@@ -36,7 +39,10 @@ export default function Paginated({ items, list, page, className }: Props) {
 
             <div className="paginated pagination">
                 <div className="pagination-wrapper">
-                    <Link to={`?page=${initialPage - 1}`} className="pagination-page">
+                    <Link
+                        to={`?page=${initialPage - 1}`}
+                        className="pagination-page"
+                    >
                         &laquo;
                     </Link>
 
@@ -45,21 +51,31 @@ export default function Paginated({ items, list, page, className }: Props) {
                               const page = initialPage - i - 1;
                               if (page < 1) return null;
                               return (
-                                  <Link key={i} to={`?page=${page}`} className="pagination-page">
+                                  <Link
+                                      key={i}
+                                      to={`?page=${page}`}
+                                      className="pagination-page"
+                                  >
                                       {page}
                                   </Link>
                               );
                           }).reverse()
                         : null}
 
-                    <span className="pagination-page current-page">{initialPage}</span>
+                    <span className="pagination-page current-page">
+                        {initialPage}
+                    </span>
 
                     {initialPage < maxPages
                         ? Array.from({ length: 2 }, (_, i) => {
                               const page = initialPage + i + 1;
                               if (page >= maxPages) return null;
                               return (
-                                  <Link key={i} to={`?page=${page}`} className="pagination-page">
+                                  <Link
+                                      key={i}
+                                      to={`?page=${page}`}
+                                      className="pagination-page"
+                                  >
                                       {page}
                                   </Link>
                               );
@@ -69,12 +85,18 @@ export default function Paginated({ items, list, page, className }: Props) {
                     <span className="pagination-page">...</span>
 
                     {initialPage !== maxPages ? (
-                        <Link to={`?page=${maxPages}`} className="pagination-page">
+                        <Link
+                            to={`?page=${maxPages}`}
+                            className="pagination-page"
+                        >
                             {maxPages}
                         </Link>
                     ) : null}
 
-                    <Link to={`?page=${initialPage + 1}`} className="pagination-page">
+                    <Link
+                        to={`?page=${initialPage + 1}`}
+                        className="pagination-page"
+                    >
                         &raquo;
                     </Link>
                 </div>
